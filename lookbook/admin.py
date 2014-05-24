89504876aab322d558d999ede1104097464ab042
jazdelu <lushizhao@qq.com>
2014-05-24 20:05:35 +0800

    Lookbook fix

--- lookbook/admin.py
+++ lookbook/admin.py
@@ -7,8 +7,8 @@ class ImageInline(admin.TabularInline):
 	extra = 1
 
 class LookbookAdmin(admin.ModelAdmin):
-	list_display = ('name',)
-	fields = ('name','description')
+	list_display = ('name','is_active')
+	fields = ('name','description',"is_active")
 
 	inlines = (ImageInline,)
 
--- lookbook/views.py
+++ lookbook/views.py
@@ -8,5 +8,5 @@ def get_lookbooks(request):
 		lookbook = Lookbook.objects.get(is_active = True)
 	except:
 		pass
-	return render_to_response('lookbook.html',{"lookbooks":lookbooks},context_instance = RequestContext(request) )
+	return render_to_response('lookbook.html',{"lookbook":lookbook},context_instance = RequestContext(request) )
 
